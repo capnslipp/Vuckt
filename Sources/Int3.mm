f6c75a714502ee40b9c2c4d173a9ffd639787d8b
Slipp Douglas Thompson <git@capnslipp.com>
2018-11-30 13:33:50 -0600

    Minor: Non-working `offsetof(…)` `static_assert` in `Int3.mm`
    * Tried adding `static_assert`s checking the struct's members' offsets against the SIMD vector's members', but Clang doesn't allow doing offset calculations on SIMD vector members.  Left the `static_assert`/`offsetof` stuff in but commented-out.  
    	⁃ `offsetof(…)` on SIMD vector spits _“error: Offsetof requires struct, union, or class type, 'simd_int3' (vector of 3 'int' values) invalid”_  
    	⁃ `&((«vector» *)0)->«member»` trick spits _“error: Address of vector element requested”_

--- Sources/Int3.mm
+++ Sources/Int3.mm
@@ -13,6 +13,13 @@ static_assert(sizeof_member(Int3, x) == sizeof_member(simd_int3, x),
 static_assert(alignof(Int3) == alignof(simd_int3),
 	"Int3's alignment requirements does not match simd_int3's on this platform.");
 
+// Doesn't work, unfortunately.  Compiler spits “error: Offsetof requires struct, union, or class type, 'simd_int3' (vector of 3 'int' values) invalid” and “error: Address of vector element requested”.
+//static_assert(
+//		offsetof(Int3, x) == offsetof(simd_int3, x) &&
+//		offsetof(Int3, y) == offsetof(simd_int3, y) &&
+//		offsetof(Int3, z) == &((simd_int3 *)0)->z,
+//	"Int3's members' offsets do not match simd_int3's members' on this platform.");
+
 
 
 @implementation NSValue (Int3Additions)
