4b0c9696fb901f39d3fbac517cc7eb9ac92c9831
Slipp Douglas Thompson <git@capnslipp.com>
2018-12-05 02:29:15 -0600

    Reorg: Moved `_ObjectiveCBridgeable` stuff into their own `.swift.gyb` files
    I was getting tired of having to run Swift's `gyb` tool frequently just to be able to run changes in `Int2`/`Int3.swift.gyb` _(since the changes needed to be GYB'd into `Int2`/`Int3.swift` in order to build/run)_.  I realized that only the `_ObjectiveCBridgeable` extensions needed to be in `.swift.gyb` files, so created new files for those extensions and un-`.gyb`-ed the rest of `Int2`/`Int3`.
    
    * Moved `_ObjectiveCBridgeable`-extension GYB code into new `Int2+_ObjectiveCBridgeable.swift.gyb` & `Int3+_ObjectiveCBridgeable.swift.gyb` files.
    	* Used Swift's `gyb` to generate new files `Int2+_ObjectiveCBridgeable.swift` & `Int3+_ObjectiveCBridgeable.swift`, and added them to the project & targets.
    	* Minor: Changed `Int2`/`3InNSValueObjCType` constants' privacy level: `private`→`fileprivate`
    * Renamed the remaining plain-Swift `Int2`/`Int3` files:  
    	⁃ `Int2.swift.gyb`→`Int2.swift`  
    	⁃ `Int3.swift.gyb`→`Int3.swift`  
    	▸ And added them to the targets.

---
+++ Sources/Int3+_ObjectiveCBridgeable.swift
@@ -0,0 +1,45 @@
+// IntN
+// @author: Slipp Douglas Thompson
+// @license: Public Domain per The Unlicense.  See accompanying LICENSE file or <http://unlicense.org/>.
+
+#if _runtime(_ObjC)
+
+fileprivate let Int3InNSValueObjCType = NSValue(int3: Int3()).objCType
+
+
+extension Int3: _ObjectiveCBridgeable {
+  public func _bridgeToObjectiveC() -> NSValue {
+    return { NSValue(int3: $0) }(self)
+  }
+
+  public static func _forceBridgeFromObjectiveC(_ source: NSValue,
+                                                result: inout Int3?) {
+    precondition(strcmp(source.objCType,
+                        { _ in Int3InNSValueObjCType }(Int3.self)) == 0,
+                 "NSValue does not contain the right type to bridge to Int3")
+    result = { $0.int3Value }(source)
+  }
+
+  public static func _conditionallyBridgeFromObjectiveC(_ source: NSValue,
+                                                        result: inout Int3?)
+      -> Bool {
+    if strcmp(source.objCType, { _ in Int3InNSValueObjCType }(Int3.self)) != 0 {
+      result = nil
+      return false
+    }
+    result = { $0.int3Value }(source)
+    return true
+  }
+
+  public static func _unconditionallyBridgeFromObjectiveC(_ source: NSValue?)
+      -> Int3 {
+    let unwrappedSource = source!
+    precondition(strcmp(unwrappedSource.objCType,
+                        { _ in Int3InNSValueObjCType }(Int3.self)) == 0,
+                 "NSValue does not contain the right type to bridge to Int3")
+    return { $0.int3Value }(unwrappedSource)
+  }
+}
+
+
+#endif // _runtime(_ObjC)
