2b0eb9a80a453c4ffb69d7ced7fc7d0715d60680
Slipp Douglas Thompson <git@capnslipp.com>
2026-04-30 16:42:14 -0600

    Added VucktTestsTarget for iOS on-device testing
    * Added a simple cross-platform app target VucktTestsTarget that does nothing, needed as a Host Application for VucktTests in order to do iOS/tvOS/etc. on-device testing.

---
+++ VucktTestsTarget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VucktTestsTarget
+//
+//  Created by Cap'n Slipp on 4/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("VucktTestsTarget")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VucktTestsTarget/VucktTestsTargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  VucktTestsTargetApp.swift
+//  VucktTestsTarget
+//
+//  Created by Cap'n Slipp on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct VucktTestsTargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
